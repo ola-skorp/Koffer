28578935750603ae0322c76ddc70ee293abc7c28
olga <ola.skorp.developer@gmail.com>
2025-03-13 09:48:30 +0100

    Initial Commit

---
+++ Koffer/KofferApp.swift
@@ -0,0 +1,17 @@
+//
+//  KofferApp.swift
+//  Koffer
+//
+//  Created by Ольга on 13.03.25.
+//
+
+import SwiftUI
+
+@main
+struct KofferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
